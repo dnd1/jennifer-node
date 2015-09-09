df94306518bb3a42a900dd54da67551493ed8fc6
¨jennifer¨ <jennifer.zambrano@dnd1.com>
2015-09-09 17:01:33 -0430

    ejercicio4.js y ejercicio5.js terminado

---
+++ ejercicio5.js
@@ -0,0 +1,16 @@
+var fs 		= require("fs");
+var directorio	= process.argv[2];
+var path	= require('path');
+fs.readdir(directorio, function( err, lista){
+     if(err) return console.error(err);
+	 ext = path.extname(lista);
+	for (var i = 0; i < ext.length; i++){
+		result = (lista[i]);
+			if(i<3){
+			   console.log(result);
+			}
+	} 
+
+	
+});
+       
